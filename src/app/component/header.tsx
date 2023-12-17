a7f48936e25867cbdac8ff3575e664b148f1eba4
6D-pixel <dinesh7rd@gmail.com>
2023-12-17 23:47:45 +0530

    add folder component and created nave bar

---
+++ src/app/component/header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className='flex w-full h-12 bg-green-800 items-center justify-between p-2'>
+        <div className='text-white'>View Highscores</div>
+        <div className=' text-white ml-auto'>Time : </div>
+    </header>
+  )
+}
